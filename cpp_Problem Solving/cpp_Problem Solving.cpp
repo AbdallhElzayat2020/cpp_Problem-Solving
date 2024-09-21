16c53d0b006c036b4eaa09adce7f555637a2415e
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-09-21 16:52:15 +0300

    Multi line Print

--- cpp_Problem Solving/cpp_Problem Solving.cpp
+++ cpp_Problem Solving/cpp_Problem Solving.cpp
@@ -19,15 +19,40 @@
 
 
 #include <iostream>
+using namespace std;
+
+//int main()
+//{
+//
+//	// print new line with std::endl
+//	cout << "abdallh" << std::endl;
+//	cout << "this is new Line ";
+//	return 0;
+//
+//	//std::cout << "Abdallh \n";
+//
+//	// new line Print and \n make new line 
+//
+//	//std::cout << "New Line \n";
+//
+//
+//
+//
+//
+//
+//}
+
+
+
+
+//print two empty lines
 
 int main()
 {
+	cout << "Abdallh" << endl << endl;
 
-	std::cout << "Abdallh";
-
-	return 0;
+	std::cout << "abdallh after 2 line empty";
 }
-
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 

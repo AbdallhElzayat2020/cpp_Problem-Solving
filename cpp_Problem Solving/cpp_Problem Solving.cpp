527ff8c1a02b50936c377af49266ec09d0fced5c
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-09-21 20:16:34 +0300

    Info Card Advanced

--- cpp_Problem Solving/cpp_Problem Solving.cpp
+++ cpp_Problem Solving/cpp_Problem Solving.cpp
@@ -47,37 +47,57 @@ using namespace std;
 
 //print two empty lines
 
-int main()
-{
-	cout << "Abdallh" << endl << endl;
-
-	std::cout << "abdallh after 2 line empty";
-}
+//int main()
+//{
+//	cout << "Abdallh" << endl << endl;
+//
+//	std::cout << "abdallh after 2 line empty";
+//}
 
 
 //#include <iostream>
+//int main() {
+//	std::cout << "M1" << "M2" << "M3 \n";
+//
+//	std::cout << "M1" << "M2" << "m3";
+//
+//	return 0;
+//}
+
+//int main() {
+//	std::cout << "*********************" << std::endl;
+//	std::cout << "Name :" << " Abdallh Elzayat \n";
+//	std::cout << "Age :" << "21 Year \n";
+//	std::cout << "Country :" << "Egypt \n";
+//	std::cout << "City :" << "Samnoud \n";
+//
+//	std::cout << "*********************";
+//
+//	//std::cout <<" the result of " << "10 + 5 = " << 10 + 5 << std::endl;
+//}
+
+
+
+//int main() {
+	//std::cout << "*********** \n";
+	//std::cout << "*********** \n";
+	//std::cout << "*********** \n";
+	//std::cout << "*********** \n";
+	//std::cout << "*********** \n";
+//}
+
 int main() {
-	std::cout << "M1" << "M2" << "M3 \n";
 
-	std::cout << "M1" << "M2" << "m3";
+	std::cout << "i Love Projramming" << std::endl;
+	std::cout << "i Promise to be a best Develoepr" << std::endl;
+	std::cout << "i Know it will taje some time to practice , but I " << std::endl;
+	std::cout << "Will achieve my goal \n \n";
 
-	return 0;
+	std::cout << "Best Regards";
+	std::cout << "Abdallh Elzayat";
+
+
 }
-
-int main() {
-	std::cout << "*********************" << std::endl;
-	std::cout << "Name :" << " Abdallh Elzayat \n";
-	std::cout << "Age :" << "21 Year \n";
-	std::cout << "Country :" << "Egypt \n";
-	std::cout << "City :" << "Samnoud \n";
-
-	std::cout << "*********************";
-
-	//std::cout <<" the result of " << "10 + 5 = " << 10 + 5 << std::endl;
-}
-
-
-
 
 
 
